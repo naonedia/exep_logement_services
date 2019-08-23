acf17a0394fafbf83df6d135fd05c92134da946a
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-08-23 14:27:21 +0200

    added script

--- model/model-scripts/retrain.py
+++ model/model-scripts/retrain.py
@@ -1,88 +1,69 @@
 import sys
+import json
 
-import tensorflow as tf
 import pandas as pd
 import numpy as np
 
+from keras import regularizers
 from keras.layers.core import Activation, Dense
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.optimizers import Adam
 
 from sklearn.model_selection import train_test_split
 
-def normalize(x_train, x_test):
+from tensorflow import set_random_seed
+
+np.random.seed(42)
+PYTHONHASHSEED=42
+set_random_seed(42)
+
+def normalize(x_train, x_test, columns):
     mu = np.mean(x_train, axis=0)
     std = np.std(x_train, axis=0)
     x_train_normalized = (x_train - mu) / std
     x_test_normalized = (x_test - mu) / std
+    
     return x_train_normalized, x_test_normalized
 
 
 def load_data(path):
-    dataset = pd.read_csv(path)
-
-    train_data, test_data = train_test_split(dataset, test_size=0.25, random_state=42)
-
+    train_data = pd.read_csv(path + '.train.csv')
+    test_data = pd.read_csv(path + '.test.csv')
+    
     X_train = train_data.drop(['valeur_fonciere'], axis=1).to_numpy()
     y_train = train_data['valeur_fonciere'].to_numpy()
     X_test = test_data.drop(['valeur_fonciere'], axis=1).to_numpy()
     y_test = test_data['valeur_fonciere'].to_numpy()
 
-    X_train_normal, X_test_normal = normalize(X_train, X_test)
+    X_train_normal, X_test_normal = normalize(X_train, X_test, train_data.drop(['valeur_fonciere'], axis=1).columns)
 
     return X_train_normal, y_train, X_test_normal, y_test
-
-
-def create_model(dataset):
-
-    # define our MLP network
-    model = Sequential()
-        
-    # Input Layer :
-    model.add(Dense(128, kernel_initializer='normal',input_dim = dataset.shape[1], activation='relu'))
-        
-    # Hidden Layers :
-    model.add(Dense(256, kernel_initializer='normal',activation='relu'))
-    model.add(Dense(512, kernel_initializer='normal',activation='relu'))
-    model.add(Dense(512, kernel_initializer='normal',activation='relu'))
-    model.add(Dense(256, kernel_initializer='normal',activation='relu'))
-    # Output Layer :
-    model.add(Dense(1, kernel_initializer='normal',activation='linear'))
-
-    opt = Adam(lr=1e-2, decay=1e-3 / 10)
-    model.compile(loss='mae', optimizer=opt, metrics=["mse", "mae"])
-
-    return model 
-
-
-def convertKerasToTensor(model, output):
-    # The export path contains the name and the version of the model
-    tf.keras.backend.set_learning_phase(0)  # Ignore dropout at inference
-
-    # Fetch the Keras session and save the model
-    # The signature definition is defined by the input and output tensors
-    # And stored with the default serving key
-    with tf.keras.backend.get_session() as sess:
-        tf.saved_model.simple_save(
-            sess,
-            output,
-            inputs={'input_data': model.input},
-            outputs={t.name: t for t in model.outputs})
-
 
 def main(argv):
     X_train, y_train, X_test, y_test = load_data(argv[0])
 
-    model = keras.load_model(argv[2])
+    # Load model
+    model = load_model(argv[0] + '.h5')
+
+    # Load old history
+    with open(argv[0] + '.history.json', 'r') as f:
+        historyOld = json.load(f)
     
-    # Train the model
+    # Re-train the model
     print("[INFO] training model...")
     history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=1000, batch_size=40000, verbose=2)
 
-    convertKerasToTensor(model, argv[1])
+    # Save model
+    model.save(argv[1] + '.h5')
+
+    # Concat old and new history
+    historyConcat = {}
+    for key in historyOld.keys():
+        historyConcat[key] = historyOld[key] + history.history[key]
+    
+    # Save concatenated history
+    with open(argv[1] + '.history.json', 'w') as f:
+        json.dump(historyConcat, f)
 
 if __name__ == '__main__':
-    
-    main(sys.argv[1:])
-    
-
+        main(sys.argv[1:])
