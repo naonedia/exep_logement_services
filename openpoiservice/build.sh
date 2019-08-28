e357bff32c951713efda6ab719acb4dfadcdc3eb
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-08-28 10:46:44 +0200

    fix: build

--- openpoiservice/build.sh
+++ openpoiservice/build.sh
@@ -7,5 +7,5 @@ docker-compose build
 
 docker-compose up -d
 
-docker exec -it naonedia_openpoi_db_1 /ops_venv/bin/python manage.py create-db
+docker exec -it naonedia_gunicorn_flask_1 /ops_venv/bin/python manage.py create-db
 docker exec -it naonedia_gunicorn_flask_1 /ops_venv/bin/python manage.py import-data
